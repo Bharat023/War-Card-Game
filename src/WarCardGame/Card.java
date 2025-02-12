2275450201da5e264b19eb7b11b858c316b6dca4
Bharat023 <bharat0@sheridancollege.ca>
2025-02-11 21:55:04 -0500

    Final

--- src/WarCardGame/Card.java
+++ src/WarCardGame/Card.java
@@ -9,66 +9,66 @@ package WarCardGame;
  * @author bhara
  */
 public class Card {
- // Example using Array
-    private int value;
-    private String suit;
-
-    public static final String[] SUITS={
-       "HEARTS", "CLUBS","SPADES", "DIAMONDS" };   
-    
-    public Card(int value, String suit)
-    {
-        this.value = value;
-        this.suit = suit;
-    }
-
-    public String getSuit() {
-        return suit;
-    }
-
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-    
-    public int getValue() {
-        return value;
-    }
-    
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-	
-//    //Example using Enum
-//    public enum Suit {
-//        HEARTS, CLUBS, SPADES, DIAMONDS
-//    }
+// // Example using Array
+//    private int value;
+//    private String suit;
 //
-//    public enum Value {
-//        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
-//        JACL
-//    }
-//    private Value value;
-//    private Suit suit;
-//
-//    public Card(Value v, Suit s) {
-//        value = v;
-//        suit = s;
-//    }
-//
-//    public Value getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(Value value) {
+//    public static final String[] SUITS={
+//       "HEARTS", "CLUBS","SPADES", "DIAMONDS" };   
+//    
+//    public Card(int value, String suit)
+//    {
 //        this.value = value;
+//        this.suit = suit;
 //    }
 //
-//    public Suit getSuit() {
+//    public String getSuit() {
 //        return suit;
 //    }
 //
-//    public void setSuit(Suit suit) {
+//    public void setSuit(String suit) {
 //        this.suit = suit;
 //    }
+//    
+//    public int getValue() {
+//        return value;
+//    }
+//    
+//    public void setValue(int value) {
+//        this.value = value;
+//    }
+
+	
+//    //Example using Enum
+   public enum Suit {
+      HEARTS, CLUBS, SPADES, DIAMONDS
+   }
+
+   public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
+        JACL
+    }
+    private Value value;
+    private Suit suit;
+
+   public Card(Value v, Suit s) {
+       value = v;
+       suit = s;
+    }
+
+   public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
 }
--- src/WarCardGame/Cardgame.java
+++ src/WarCardGame/Cardgame.java
@@ -24,8 +24,12 @@ public class Cardgame {
         Random random = new Random();
         for (int i = 0; i < hand.length; i++) 
         {
-            int value = random.nextInt(13)+1;
-            String suit = Card.SUITS[random.nextInt(4)];
+//            int value = random.nextInt(13)+1;
+//            String suit = Card.SUITS[random.nextInt(4)];
+            Card.Value value = Card.Value.values()[random.nextInt(Card.Value.values().length)];
+            Card.Suit suit = Card.Suit.values()[random.nextInt(Card.Suit.values().length)];
+
+            
             Card card = new Card(value, suit);
             hand[i] = card;
         }
@@ -36,17 +40,23 @@ public class Cardgame {
 
         //ask user for Card
         System.out.println("Please choose a suit for your lucky card:");
-        for (int i = 0; i < Card.SUITS.length; i++) {
-            System.out.println((i + 1) + ": " + Card.SUITS[i]);
+//        for (int i = 0; i < Card.SUITS.length; i++) {
+//            System.out.println((i + 1) + ": " + Card.SUITS[i]);
+//        }
+        Card.Suit[] suits = Card.Suit.values();
+        for (int i = 0; i < suits.length; i++) {
+            System.out.println((i + 1) + ": " + suits[i]);
         }
         
         int suit = input.nextInt();
         System.out.println("Enter a value (1 to 13)");
         int value = input.nextInt();
         //create new Card with the two values chosen
-        Card userGuess = new Card(value,Card.SUITS[suit-1]);
-
-        //check if it matches
+//        Card userGuess = new Card(value,Card.SUITS[suit-1]);
+//
+ Card userGuess = new Card(Card.Value.values()[value], Card.Suit.values()[suit]);
+       
+ //check if it matches
         boolean match = false;
         for (Card card : hand) {
             if (card.getValue() == userGuess.getValue()
